4c6c9803f99e485b73ff9360bcd20cb196c41e5e
Justin <justinrobertsdw@gmail.com>
2019-09-24 15:58:03 -0600

    update

--- noaaParser/main.cpp
+++ noaaParser/main.cpp
@@ -22,12 +22,12 @@ int main(int argc, char* argv[]) {
 	Parser parser(printFile);
 	
 	// other test files
-	//string filename = "C:/Users/A10/Documents/Justins/PKM00041780-data.txt"; // all years for station PKM00041780
-	//string filename = "C:/Users/A10/Documents/Justins/USW00013859-data.txt"; // all years for station USW00013859
-	//string filename = "C:/Users/A10/Documents/Justins/ZZV00ASEU05-data.txt"; // all years for station ZZV00ASEU05
-	//string filename = "C:/Users/A10/Documents/Justins/ACM00078861-data.txt"; // all years for station ACM00078861
-	//string filename = "C:/Users/A10/Documents/Justins/AEM00041217-data.txt"; // ytd for station AEM00041217
-	//string filename = "C:/Users/A10/Documents/Justins/ACM00078861-data_header.txt"; // only one data block for station ACM0078861
+	//string filename = "C:/Users/A10/Documents/Justins/noaaData/PKM00041780-data.txt"; // all years for station PKM00041780
+	//string filename = "C:/Users/A10/Documents/Justins/noaaData/USW00013859-data.txt"; // all years for station USW00013859
+	//string filename = "C:/Users/A10/Documents/Justins/noaaData/ZZV00ASEU05-data.txt"; // all years for station ZZV00ASEU05
+	//string filename = "C:/Users/A10/Documents/Justins/noaaData/ACM00078861-data.txt"; // all years for station ACM00078861
+	//string filename = "C:/Users/A10/Documents/Justins/noaaData/AEM00041217-data.txt"; // ytd for station AEM00041217
+	//string filename = "C:/Users/A10/Documents/Justins/noaaData/ACM00078861-data_header.txt"; // only one data block for station ACM0078861
 
 	ifstream is(filename);
 
